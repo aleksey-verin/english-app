c925e6bcd0215a131ced4f08c3aa820ee4b7f75a
Aleksey Verin <verevaa@yandex.ru>
2023-03-28 15:54:03 +0300

    change routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,6 @@
-export const LOGIN_ROUTE = '/login';
-export const DICTIONARY_ROUTE = '/dictionary';
-export const WORDS_ROUTE = '/words';
-export const TRAINING_ROUTE = '/training';
+const BASE_ROUTE = '/english-app';
+
+export const LOGIN_ROUTE = `${BASE_ROUTE}/login`;
+export const DICTIONARY_ROUTE = `${BASE_ROUTE}/dictionary`;
+export const WORDS_ROUTE = `${BASE_ROUTE}/words`;
+export const TRAINING_ROUTE = `${BASE_ROUTE}/training`;
